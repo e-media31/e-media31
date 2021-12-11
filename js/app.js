c0cb7f9398b1d8044dd2174d2d0ed952e2d5a54a
e-media31 <6159859+e-media31@users.noreply.github.com>
2021-12-11 11:38:42 +0100

    fix : app.js  catch

--- js/app.js
+++ js/app.js
@@ -36,7 +36,7 @@ if ("serviceWorker" in navigator) {
     navigator.serviceWorker
       .register("serviceWorker.js")
       .then(res => console.log("service worker registered"))
-      .catch(err => console.log("service worker not registered", err));
+      .catch( (err) => {console.log("service worker not registered", err)} );
   });
 };
 
