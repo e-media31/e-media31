e9e823d3a787024966ccf0accee03e65781707ee
e-media31 <6159859+e-media31@users.noreply.github.com>
2021-12-11 11:30:49 +0100

    fix : serviceworker.js 2

--- serviceWorker.js
+++ serviceWorker.js
@@ -16,22 +16,6 @@ const assets = [
   "/images/coffee9.jpg",
 ];
 
-self.addEventListener("install", installEvent => {
-  installEvent.waitUntil(
-    caches.open(staticDevCoffee).then(cache => {
-      cache.addAll(assets)
-    })
-  );
-});
-
-
-self.addEventListener("fetch", fetchEvent => {
-  fetchEvent.respondWith(
-    caches.match(fetchEvent.request).then(res => {
-      return res || fetch(fetchEvent.request)
-    })
-  );
-});
 
 alert('sw done');
 
